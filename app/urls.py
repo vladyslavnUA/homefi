db80da35f81f809568f88e9df21648fe1b5fb48e
vladyslavnUA <vladyslavn@yahoo.com>
2020-12-05 01:40:57 -0500

    updated urls

--- app/urls.py
+++ app/urls.py
@@ -1,8 +1,13 @@
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 from .views import *
 
 urlpatterns = [
     path('', home, name='home'),
     path('about/', about, name='about'),
     path('detail/', detail, name='detail'),
-]
+    path('<int:id>/', single, name='single'),
+    path('new-listing/', new_listing, name='new_listing'),
+    path('edit-listing/<int:id>/', edit_listing, name='edit_listing'),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
